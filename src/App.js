886ef5ea6adf0e375f21dd39f3dcc75d1b4492fd
Balan Nicoleta <balan_nicky@yahoo.com>
2020-05-10 19:38:12 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import LOperatori from "./operatori";
+import CorpTabel from "./corptabel";
 import "./App.css";
 
 class App extends Component {
@@ -16,20 +17,38 @@ class App extends Component {
   }
 
   corectezOper(ev) {
-    const idSup = parseInt(ev.target.id);  //  id e convertit in intreg
+    const idSup = parseInt(ev.target.id); //  id e convertit in intreg
   }
 
   stergOper(ev) {
-    const idSup = parseInt(ev.target.id);  //  id e convertit in intreg
+    const idSup = parseInt(ev.target.id); //  id e convertit in intreg
   }
 
   render() {
-    this.state.operatori.map(item => console.log(`Utilizator: ${item.nume}`));
+    this.state.operatori.map((item) => console.log(`Utilizator: ${item.nume}`));
     return (
       <div className="container">
         <h2 className="text-center">
           Lista operatorilor aplicației <em>ALEGERI</em>
         </h2>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Utilizator</th>
+              <th scope="col">Nume și prenume</th>
+              <th scope="col">Email</th>
+              <th scope="col">Locație</th>
+              <th scope="col">Operații</th>
+            </tr>
+          </thead>
+          <tbody>
+            <CorpTabel
+              operatori={this.state.operatori}
+              corectezOper={this.corectezOper}
+              stergOper={this.stergOper}
+            />
+          </tbody>
+        </table>
       </div>
     );
   }
